7190e9b83042cc702d1f943965ade6fc4a4a0d0f
Shanise Barona <shanisebarona@gmail.com>
2018-05-30 02:54:49 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -16,4 +16,19 @@
 #   min
 # end
 
+def key_for_min_value(name_hash)
+  min = nil
+  num = 0
+    name_hash.collect do |k, v|
+    if min == nil 
+      min = k 
+      num = v
+        elsif v < num 
+        num = v
+        min = k
+      end  
+    end
+  min
+end
+
     
